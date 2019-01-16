6ddc1999390833ecce72bd726931db443cd95f3f
maiccom <maiko.matsuda@gmail.com>
2019-01-16 17:05:38 +0900

    Lesson 7の課題 GoFowardUnityChan
    AudioSource.PlayClipAtPointを使ってキューブが地面に接触する時とキューブ同士が積み重なるときに効果音が鳴るようにした。

---
+++ Assets/HitPlaySound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour
+{
+
+    public AudioClip sound;
+    public float volume = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //ブロックの効果音を鳴らす
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //ブロックが床に、又はほかのブロックに衝突した場合、音を再生する
+        if (other.gameObject.tag == "GroundTag" || other.gameObject.tag == "CubeTag")
+        {
+            AudioSource.PlayClipAtPoint(sound, transform.position, volume);
+        }
+    }
+}
